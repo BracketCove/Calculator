993e588b4ace5135ee9e37ade4f7deb5d4d95b70
BracketCove <bracket.cove@gmail.com>
2016-10-23 12:25:50 -0700

    Commit for Lesson 1.2 - Color Scheme and Themes

---
+++ app/src/main/java/com/wiseass/calculator/CalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.wiseass.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
