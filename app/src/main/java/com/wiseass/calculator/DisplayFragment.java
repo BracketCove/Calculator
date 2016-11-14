9b16cd243c5c7d8dc18c3c2e572c854fad125791
BracketCove <bracket.cove@gmail.com>
2016-11-14 11:11:00 -0800

    Commit for lesson 2.1 - ButterKnife and Writing our View (as in Model-*View*-Presenter

--- app/src/main/java/com/wiseass/calculator/DisplayFragment.java
+++ app/src/main/java/com/wiseass/calculator/DisplayFragment.java
@@ -6,24 +6,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnLongClick;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DisplayFragment extends Fragment {
+    @BindView(R.id.lbl_display)
+    TextView display;
 
+    @OnClick(R.id.imb_delete)
+    public void onDeleteShortClick(View v){
+
+    }
+
+    @OnLongClick(R.id.imb_delete)
+    public void onDeleteLongClick(View v){
+
+    }
 
     public DisplayFragment() {
         // Required empty public constructor
     }
 
+    public static DisplayFragment newInstance(){
+        return new DisplayFragment();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display, container, false);
+        View v = inflater.inflate(R.layout.fragment_display, container, false);
+        ButterKnife.bind(this, v);
+        return v;
     }
 
 }
--- app/src/main/java/com/wiseass/calculator/InputFragment.java
+++ app/src/main/java/com/wiseass/calculator/InputFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
@@ -13,17 +17,49 @@ import android.view.ViewGroup;
  */
 public class InputFragment extends Fragment {
 
+    @OnClick({R.id.btn_number_one, R.id.btn_number_two, R.id.btn_number_three, R.id.btn_number_four,
+            R.id.btn_number_five, R.id.btn_number_six, R.id.btn_number_seven, R.id.btn_number_eight,
+            R.id.btn_number_nine, R.id.btn_number_zero})
+    public void onNumberClick(Button v){
+
+    }
+
+    @OnClick({R.id.btn_operator_add, R.id.btn_operator_divide, R.id.btn_operator_multiply,
+            R.id.btn_operator_subtract})
+    public void onOperatorClick(Button v){
+
+    }
+
+    @OnClick(R.id.btn_decimal)
+    public void onDecimalClick(Button v){
+
+    }
+
+    @OnClick(R.id.btn_evaluate)
+    public void onEvaluateClick(Button v){
+
+    }
 
     public InputFragment() {
         // Required empty public constructor
     }
 
+    public static InputFragment newInstance(){
+        return new InputFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false);
+        View v = inflater.inflate(R.layout.fragment_input, container, false);
+        ButterKnife.bind(this, v);
+
+        return v;
     }
 
 }
